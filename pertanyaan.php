140152c97fd8f8fd88e05b031d068f0cac09e145
AriniInf <36852214+AriniInf@users.noreply.github.com>
2019-05-19 14:38:16 +0700

    Kuraang rapi maafkan

--- pertanyaan.php
+++ pertanyaan.php
@@ -21,28 +21,309 @@
 <br>
 <br>
 <br>
-<div class="btn-toolbar text-center well">
-    <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm col-xs-2 margin-left">
-      <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> VIEW NAJA 1
-    </button>
-    <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm col-xs-2 margin-left">
-      <span class="glyphicon" aria-hidden="true"></span> VIEW NAJA 2
-    </button>
-    <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm col-xs-2 margin-left">
-      <span class="glyphicon" aria-hidden="true"></span> VIEW ARINI 1
-    </button>
-    <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm col-xs-2 margin-left">
-      <span class="glyphicon" aria-hidden="true"></span> VIEW ARINI 2
-    </button>
-    <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm col-xs-2 margin-left">
-      <span class="glyphicon glyphicon-edit" aria-hidden="true"></span> EDIT CUSTOMER
-    </button>
-    <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm col-xs-2 margin-left">
-      <span class="glyphicon glyphicon-time" aria-hidden="true"></span> HISTORY
-    </button>
-    <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm col-xs-2 margin-left">
-      <span class="glyphicon glyphicon-trash" aria-hidden="true"></span> DELETE CUSTOMER
-    </button>
+<table class="table table-bordered">
+    <thead>
+      <tr>
+      <th scope="col">No</th>
+      <th scope="col">Pertanyaan Ulil</th>
+      <th scope="col">OUTPUT</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tampilkan ruang yang tidak disewa di fakultas FTIK pda seminggu kedepan</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> VIEW 1
+          </button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Tampilkan ruang yang paling sering disewa</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> VIEW 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Buatlah Function untuk mengecek apakah suatu ruang pada tanggal dan jam tertentu kosong atau tidak</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> FUNCTION 1
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Buatlah Function untuk menghitung jumlah ruangan yang berproyektor</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> FUNCTION 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Buatlah Procedure untuk merubah semua jam buka dari semua ruang pada hari tertentu</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> PROCEDURE 1
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Buatlah Procedure untuk meningkatkan harga per jam dari suatu ruang sebanyak 20%</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> PROCEDURE 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Buatlah Trigger untuk Insert transaksi, dimana transaksi tidak dapat dilakukan apabila ruangan tidak kosong</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> TRIGGER 1
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Buatlah Trigger untuk setiap update gaji pada tabel karyawan untuk menampilkan perubahan gaji</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> TRIGGER 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>Tampilkan nama karyawan yang menangani transaksi ruang yang memiliki proyektor (inner)</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> JOIN 1
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>Tampilkan ruang yang buka mulai jam 7 (outer)</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> JOIN 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">11</th>
+      <td>Buatlah index pada tabel penyewa kolom penyewa berdasarkan 3 huruf pertamanya</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> INDEX
+      </td>
+    </tr>
+    </tbody>
+  </table>
+
+<table class="table table-bordered">
+    <thead>
+      <tr>
+      <th scope="col">No</th>
+      <th scope="col">Pertanyaan Arini</th>
+      <th scope="col">OUTPUT</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tampilkan penyewa yang menyewa pada bulan Oktober</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> VIEW 1
+          </button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Tampilkan karyawan yang menerima transaksi yang menyewa ruang di gedung pusat robotika</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> VIEW 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Buatlah function untuk menghitung total biaya sewa ruang dengan total harga + denda pada penyewa jika terkena denda dengan input id_penyewa</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> FUNCTION 1
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Buatlah fungsi untuk menghitung total pemasukan dengan input bulan dan tahun tertentu dengan asumsi pemasukan didapat dari tanggal masuk transaksi.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> FUNCTION 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Buatlah procedure untuk mengupdate gaji sebesar 10% pada table karyawan dengan total transaksi lebih dari 10.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> PROCEDURE 1
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Buatlah Procedure untuk memberikan potongan harga sebesar 10% pada table orang yang menyewa lebih dari 15x</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> PROCEDURE 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Buatlah tabel log yang dapat menyimpan histori tabel transaksi update simpan juga tanggal dan statusnya simpan data sebelum dan setelah data di-update.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> TRIGGER 1
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Buatlah tabel log yang dapat menyimpan histori tabel transaksi delete simpan juga tanggal dan statusnya.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> TRIGGER 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>Tampilkan nama penyewa beserta karyawan yang melayaninya beserta tanggal penyewaan-nya.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> JOIN 1
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>Tampilkan semua ruang dan nama penyewa yang pernah menyewa ruang tersebut.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> JOIN 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">11</th>
+      <td>Buatlah index tabel Karyawan kolom nama_karyawan berdasarkan 3 huruf pertamanya.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> INDEX
+      </td>
+    </tr>
+    </tbody>
+  </table>
+
+
+<table class="table table-bordered">
+    <thead>
+      <tr>
+      <th scope="col">No</th>
+      <th scope="col">Pertanyaan Ihdiannaja</th>
+      <th scope="col">OUTPUT</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Tampilkan nama penyewa yang memiliki tanggungan denda</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> VIEW 1
+          </button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Tampilkan ID dan nama penyewa beserta jumlah denda yang pernah didapatkan.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> VIEW 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Buatlah function untuk melakukan potongan total harga sebesar 10% pada peminjaman yang memiliki total harga paling besar. </td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> FUNCTION 1
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Buatlah function untuk melakukan penambahan kapasitas sebesar 10 pada ruang yang paling kecil.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> FUNCTION 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Buatlah procedure untuk menampilkan total jam peminjaman dengan input id penyewa.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> PROCEDURE 1
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Buatlah procedure untuk menampilkan jadwal peminjaman pada ruangan yang diinputkan.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> PROCEDURE 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Buatlah trigger setiap terjadi perubahan pada tabel ruang ketika kapasitasnya lebih dari 30. </td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> TRIGGER 1
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Buatlah trigger setiap terjadi perubahan data pada tabel penyewa.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> TRIGGER 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>Tampilkan semua nama karyawan beserta transaksi yang pernah dilakukan.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> JOIN 1
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>Tampilkan ID dan nama penyewa yang terkena denda. </td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> JOIN 2
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">11</th>
+      <td>Buatlah index pada tabel RUANG kolom kode_ruang berdasarkan 3 huruf pertamanya.</td>
+      <td><a href="#">  
+          <button type="button" class="btn btn-primary btn-color btn-bg-color btn-sm margin-left">
+          <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> INDEX
+      </td>
+    </tr>
+    </tbody>
+  </table>
 </div>
 </head>
 </html>
