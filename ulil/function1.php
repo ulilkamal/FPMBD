bbdff05d9c5c0bfc6b230ccf1a0547bc774fb367
ulilkamal <ulilkml@gmail.com>
2019-05-19 21:39:07 +0700

    view sama function

---
+++ ulil/function1.php
@@ -0,0 +1,98 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+    <title>Update Data</title>
+       
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="../style.css">
+          
+</head>
+<body>
+  
+    <!-- container -->
+    <div class="container">
+        <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div class="btn-toolbar text-center well" >
+            <h1>Function 1 Ulil</h1>
+        </div>
+      
+        <?php
+
+require '../config/database.php';
+if(isset($_POST['cek'])){
+ 
+    // include database connection
+    include '../config/database.php';
+ 
+    try{
+        // insert query
+        $query = "SELECT cek(:ruang,:tanggal,:mulai,:selesai) as hasil";
+        //prepare query for execution
+        $stmt = $conn->prepare($query);
+        $ruang = htmlspecialchars(strip_tags($_POST['ruang']));
+        $tanggal = htmlspecialchars(strip_tags($_POST['tanggal']));
+        $mulai = htmlspecialchars(strip_tags($_POST['mulai']));
+        $selesai = htmlspecialchars(strip_tags($_POST['selesai']));
+        
+        $stmt->bindParam(':ruang', $ruang);
+        $stmt->bindParam(':tanggal', $tanggal);
+        $stmt->bindParam(':mulai', $mulai);
+        $stmt->bindParam(':selesai', $selesai);
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);  
+        $value = $row['hasil'];
+        if($value==0) echo "<div class='alert alert-danger'>Ruangan Terisi.</div>";
+        else if ($value==1) echo "<div class='alert alert-success'>Ruangan Kosong.</div>";
+        
+    }
+     
+    // show error
+    catch(PDOException $exception){
+        die('ERROR: ' . $exception->getMessage());
+    }
+}
+?>
+<form  method="post">
+    <table class='table table-hover table-responsive table-bordered'>
+        <tr>
+            <td>Ruang</td>
+            <td><input type='text' name='ruang' class='form-control' /></td>
+        </tr>
+        <tr>
+            <td>Tanggal</td>
+            <td><input type='text' name='tanggal' class='form-control' /></td>
+        </tr>
+        <tr>
+            <td>Jam Mulai</td>
+            <td><input type='text' name='mulai' class='form-control' /></td>
+        </tr>
+        <tr>
+            <td>Jam Selesai</td>
+            <td><input type='text' name='selesai'  class='form-control' /></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type='submit' name='cek' value='cek' class='btn btn-primary' />
+                <!-- <a href='index.php' class='btn btn-danger'>Back to read products</a> -->
+            </td>
+        </tr>
+    </table>
+</form>
+    </div>
+    </div>
+    </div> 
+    </div> <!-- end .container -->
+      
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+   
+<!-- Latest compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+</body>
+</html>
---
+++ ulil/view1.php
@@ -0,0 +1,115 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Index</title>
+     
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+         
+    <!-- custom css -->
+    <style>
+    .m-r-1em{ margin-right:1em; }
+    .m-b-1em{ margin-bottom:1em; }
+    .m-l-1em{ margin-left:1em; }
+    .mt0{ margin-top:0; }
+    </style>
+ 
+</head>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="home.php">SEWA RUANG</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="index.php">INPUT</a></li>
+      <li><a href="pertanyaan.php">OUTPUT</a></li>
+    </ul>
+  </div>
+</nav>
+<br>
+<br>
+<br>
+<body>
+    <!-- container -->
+    <div class="container">
+        <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div class="btn-toolbar text-center well" >  
+    <h1>Soal View 1 Ulil</h1>
+        </div>
+     
+        <?php
+            // include database connection
+            include '../config/database.php';
+            $quer = "describe lihat7hari";
+            $result = $conn->prepare($quer);
+            $result->execute();
+            $n = $result->rowCount();
+
+            // select all data
+            $query = "SELECT * FROM lihat7hari";
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+
+            // this is how to get number of rows returned
+            $num = $stmt->rowCount();
+
+            // link to create record form
+
+            //check if more than 0 record found
+            if($num>0){
+
+                
+
+                echo "<table class='col-md-4 col-form-label text-md-right table table-hover table-responsive table-bordered'>";//start table
+ 
+                    //creating our table heading
+                    $result->execute();
+                    echo "<tr>";
+                        while ($row = $result->fetch()) {
+                            if ($row["Field"] != "id") {
+                                echo "<th>".$row["Field"]."</th>";
+                            }
+                        }
+                    echo "</tr>";
+                    
+                    // retrieve our table contents
+                    // fetch() is faster than fetchAll()
+                    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                        
+                         echo "<tr>";
+                                $j=0;
+                                foreach ($row as $key => $value){
+                                    if($j==0) {$namakolom=$key;$id = $value;$j++;}
+                                    echo "<td>".$value."</td>";
+                                }
+                         echo "</tr>";
+                    }
+                
+                // end table
+                echo "</table>";
+                
+            }
+            // if no records found
+            else{
+                echo "<div class='alert alert-danger'>No records found.</div>";
+            }
+        ?>
+
+     </div>
+     </div>
+    </div> <!-- end .container -->
+     
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+   
+<!-- Latest compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ 
+<script type='text/javascript'>
+// confirm record deletion
+ 
+</body>
+</html>
